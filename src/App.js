23ca76004297a89c4c98dbb3f8b5e4ad6fa398d0
Rohit <saha78605@gmail.com>
2023-08-07 22:10:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [userInput, setUserInput] = useState('');
+  const [chatLog, setChatLog] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!userInput) return;
+
+    try {
+      const response = await fetch('http://localhost:5000/chatbot', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({ prompt: userInput }),
+});
+
+
+      const data = await response.json();
+      setChatLog((prevChatLog) => [
+        ...prevChatLog,
+        { user: userInput, bot: data.response },
+      ]);
+
+      setUserInput('');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 >Chatbot App</h1>
+      <div className="chat-container">
+        {chatLog.map((entry, index) => (
+          <div key={index}>
+            <p><strong>You:</strong> {entry.user}</p>
+            <p><strong>Chatbot:</strong> {entry.bot}</p>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+          placeholder="Type your query..."
+        />
+        <button type="submit">Send</button>
+      </form>
     </div>
   );
 }
